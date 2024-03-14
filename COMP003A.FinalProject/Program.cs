7268bc0ae8230e15f5d5839e182a6daea44e4054
Matthew Hudson <matthew41305@gmail.com>
2024-03-13 17:09:58 -0700

    revision first commit

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -3,6 +3,7 @@
  * Course: COMP-003A
  * Purpose: Code for Final Project - BMI Calculator
  */
+// TODO: missing exception handling
 namespace COMP003A.FinalProject
 {
     internal class Program
@@ -34,7 +35,7 @@ namespace COMP003A.FinalProject
             double[] heightArray = GetValidatedHeight("Enter your height (ex. 5 8 --> feet, inches): ");
             double weightPounds = GetValidatedDouble("Enter your weight in pounds (example 165lbs --> 165): ");
 
-  
+
 
             // Bmi calculator
             double bmi = CalculateBMI(heightArray[0], heightArray[1], weightPounds);
@@ -73,6 +74,7 @@ namespace COMP003A.FinalProject
             // Loop to get non-null responses
             for (int i = 0; i < questions.Length; i++)
             {
+                // TODO: move this code block in a method
                 string response;
                 do
                 {
@@ -94,6 +96,9 @@ namespace COMP003A.FinalProject
 
             // Display responses
             Console.WriteLine("".PadRight(100, '*'));
+            // TODO: what is the purpose of this for loop?
+            // TODO: what is the purpose of activityLevelResponse.Count?
+            // TODO: why is i initialized to 0?
             for (int i = 0; i < activityLevelResponse.Count; i++)
             {
                 Console.WriteLine($"{questions[i]} {activityLevelResponse[i]}");
@@ -102,29 +107,29 @@ namespace COMP003A.FinalProject
 
             Console.WriteLine($"BMI: {bmi:F2}"); // gives calculated value to user
             Console.WriteLine($"BMI Index: {GetBMICategory(bmi)}");
-            Console.WriteLine("".PadRight(100, '*'));
-            Console.WriteLine("Based on your BMI refer to the following chart: ");
-            Console.WriteLine("Standard weight; continue eating maintence calories");
-            Console.WriteLine("Overweight; reduce daily caloric intake by 300 calories (caloric deficit)");
-            Console.WriteLine("Underweight; increase daily caloric intake by 300 calories (caloric surplus)");
-            Console.WriteLine("".PadRight(100, '*'));
-
+            // TODO: consider putting these BMI note in a method
 
         }
 
+        // TODO: add xml comments - Done
         // Get user's age, age calculator section
+        /// <summary>
+        /// Gets user's input for their birth year and subtracts the current year
+        /// </summary>
+        /// <param name="CalculateAge">integer input</param>
+        /// <returns> User's age</returns>
         static int CalculateAge(int birthYear)
         {
-            int currentYear = DateTime.Now.Year;
-            return currentYear - birthYear;
+            int currentYear = DateTime.Now.Year; //establishes int for current year
+            return currentYear - birthYear; // subtracts current year from given year to get age
         }
+
 
         /// <summary>
         /// Age calculator that takes the current the are the user inputed age and subtracts them
         /// </summary>
         /// <param name="CalculateAge"> integer input and output</param>
         /// <returns> the user's age</returns>
-
         static string GetValidatedName(string prompt)
         {
             string name;
@@ -145,12 +150,12 @@ namespace COMP003A.FinalProject
 
             return name;
         }
+        /// Done
         /// <summary>
-        /// loop that makes the User unable to type in null responses 
+        /// Get's user's name without allowing special character responses or integers, but it is not case sensitive
         /// </summary>
-        /// <param name="GetValidatedName"> string response for the user's name</param>
-        /// <returns> name, not case sensitive but doesn't involve special characters</returns>
-
+        /// <param name="IsAlpha"> string response for the user's name</param>
+        /// <returns> user's name, not case sensitive but doesn't involve special characters</returns>
         static bool IsAlpha(string input) //IsAlpha is used for just character responses
         {
             foreach (char c in input)
@@ -162,19 +167,22 @@ namespace COMP003A.FinalProject
             }
             return true;
         }
-        /// <summary>
-        /// allows the user to only use character inputs
-        /// </summary>
-        /// <param name="Name section"> string input</param>
-        /// <returns> name restricted to only character oriented responses</returns>
 
+        /// <summary>
+        /// gets user's input for their birth yeaer but doesn't allow years before 1900 or past the year 2024 (current year)
+        /// </summary>
+        /// <param name="GetValidatedBirthYear"> string input</param>
+        /// <returns>User's birthyear</returns>
         static int GetValidatedBirthYear(string prompt)
         {
             int birthYear;
             do
             {
                 Console.Write(prompt);
-                if (int.TryParse(Console.ReadLine(), out birthYear) && birthYear >= 1900) // sets year to be greater than or equal to the year 1900
+                // TODO: add a condition so it also doest not accept future years
+                // TODO: what is the purpose of int.Tryparse()? - The purpose of int.Tryparse is to store the user's input and parses it as an integer
+                // TODO: what is the purpose of the out keyword? The purpose of the out keyword is to 
+                if (int.TryParse(Console.ReadLine(), out birthYear) && birthYear >= 1900 && birthYear <= 2024) // set the year range choice to 1900-2024
                 {
                     break;
                 }
@@ -186,15 +194,18 @@ namespace COMP003A.FinalProject
 
             return birthYear;
         }
+
+        // TODO: revise this xml comment for GetValidatedHeight - done
         /// <summary>
-        /// gets the user's birth year and stores the value allowing it to be used the age calculator
+        /// get's the user's height in the form (x x, feet and inches) doesn't accept values 0 0, stores for later calculation (BMI)
         /// </summary>
-        /// <param name="GetValidatedBirthYear"> integer input</param>
-        /// <returns>the user's birth year allowing only integer related responses</returns>
+        /// <param name="GetValidatedHeight"> integer input</param>
+        /// <returns>the user's heigh in feet and inches separated by a space</returns>
 
         static double[] GetValidatedHeight(string prompt)
         {
             double[] heightArray;
+            // TODO: add a condition not to allow height 0 0
             do
             {
                 Console.Write(prompt);
@@ -213,33 +224,49 @@ namespace COMP003A.FinalProject
             return heightArray;
         }
 
+        // TODO: add xml comments
+        /// <summary>
+        /// array for user's input in feet and inches (parses user response), doesn't allow responses such as 0 0
+        /// </summary>
+        /// <param name="input"> integer input</param>
+        /// <returns> User's height separated by a single space (ex. 5 8 )</returns>
         static bool TryParseHeight(string input, out double[] heightArray)
         {
             heightArray = new double[2];
-            string[] parts = input.Split(' '); // this allows the feet and inches to be separated by the '
+            string[] parts = input.Split(' '); // this allows the feet and inches to be separated by the space
 
-            if (parts.Length == 2 && double.TryParse(parts[0], out heightArray[0]) && double.TryParse(parts[1], out heightArray[1]))
+            // TODO: what is the purpose of this if statement?
+            // TODO: what is the purpose of double.TryParse()?
+            // TODO: what is the purpose of the out keyword?
+            // TODO: what is the purpose of && operator? The purpose of && in this context is to connect added conditions within the array, for example not allowing values such as 0 0,
+            if (parts.Length == 2 && double.TryParse(parts[0], out heightArray[0]) && double.TryParse(parts[1], out heightArray[1]) && (heightArray[0] != 0 || heightArray[1] != 0))
+                
+             /// (heightArray[0] != 0 || heightArray[1] != 0)) condition that doesn't allow user input to be 0, 0, ends off with a double )) to close it
+
             {
                 return true;
             }
             return false;
         }
 
+        // TODO: revise this xml comment for GetValidatedDouble
         /// <summary>
-        /// get user's input for feet an inches and separates them by '
+        /// gets User's input for for weight greater than 50 lbs, but less than 1000 lbs stores for later calculation (BMI)
         /// </summary>
-        /// <param name="heightArray"> integer input</param>
-        /// <returns> height separated by a ' ex. 5'8</returns>
-
-
-
+        /// <param name="GetValidatedDouble"> integer input</param>
+        /// <returns> User's weight based on the following parameters</returns>
         static double GetValidatedDouble(string prompt)
         {
             double value;
+            // TODO: add a condition not to allow 0 lbs input - Done
             do
             {
                 Console.Write(prompt);
-                if (double.TryParse(Console.ReadLine(), out value) && value >= 0)
+                // TODO: what is the purpose of double.TryParse()?
+                // TODO: what is the purpose of the out keyword? The purpose of out in this context is to ex
+                // TODO: what is the purpose of && operator? The && operature sets the restriction on what values can be inputted by the user,
+                // in this case it doesn't allow responses lower than 50 lbs or responses over 1000
+                if (double.TryParse(Console.ReadLine(), out value) && value >= 50 && value <= 1000)
                 {
                     break;
                 }
@@ -252,11 +279,15 @@ namespace COMP003A.FinalProject
 
             return value;
         }
+
+        // TODO: revise this xml comment for CalculatBMI - Done
         ///<summary>
-        /// Causes the user to enter a value greater than 0, no negative values
+        /// calulates the user's bmi by taking previous information (height, weight) 
+        /// the double first converts the feet into inches and adds the other inches
+        /// calulates final Bmi
         /// </summary>
-        /// <param name="heightArray"> integer input</param>
-        /// <returns> integer value that is greater than 0</returns>
+        /// <param name="CalculateBMI"> integer output</param>
+        /// <returns>User's Bmi based on gathered info</returns>
 
         static double CalculateBMI(double heightFeet, double heightInches, double weightPounds)
         {
@@ -265,7 +296,14 @@ namespace COMP003A.FinalProject
             return (weightPounds / (heightInInches * heightInInches)) * 703;
         }
 
+        // TODO: add xml comments - Done
         // Get user's gender, gender section
+        /// <summary>
+        /// get's user's gender through M,F,O and returns Male, Female, or Other
+        /// </summary>
+        /// <param name="GetValidatedGender">string input</param>
+        /// <returns>User's Gender in name form, m = male, f = female, o = other</returns>
+        
         static string GetValidatedGender(string prompt)
         {
             string gender;
@@ -293,22 +331,24 @@ namespace COMP003A.FinalProject
 
             return gender;
         }
-            /// <summary>
-            /// requests users for their gender, inputing a case sensitive M,F,O that later coverts to Male, Female, or Other
-            /// </summary>
-            /// <param name="GetValidatedGender">string input</param>
-            /// <returns> User's Gender in name form, m = male, f = female, o = other</returns>
-            
-            static string GetBMICategory(double bmi)
-            {
-                if (bmi < 18.5)
-                    return "Underweight";
-                else if (bmi < 24.9)
-                    return "Normal Weight";
-                else if (bmi < 29.9)
-                    return "Overweight";
-                else
-                    return "Obese";
-            }
+
+        // TODO: revise this xml comment for GetBMICategory - Done
+        /// <summary>
+        /// After the bmi is outputted, the following executes deciding user's subject weight class
+        /// </summary>
+        /// <param name="GetValidatedBMI">integer and string output</param>
+        /// <returns> Underweight, Normal Weight, Overweight or obese, and the adjustments to their daily caloric intake</returns>
+
+        static string GetBMICategory(double bmi)
+        {
+            if (bmi < 18.5)
+                return "Underweight; increase daily caloric intake by 200-500 calories (caloric surplus)";
+            else if (bmi < 24.9)
+                return "Normal Weight; continue eating maintence calories";
+            else if (bmi < 29.9)
+                return "Overweight; reduce daily caloric intake by 300 calories (caloric deficit)";
+            else
+                return "Obese; reduce daily caloric intake by 300-500 calories (caloric deficit)";
         }
     }
+}
